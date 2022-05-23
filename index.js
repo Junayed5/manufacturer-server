b51b665b56085b8c56d8618a4a83995795115a5e
Junayed5 <mdjunayed352003@gmail.com>
2022-05-23 16:05:31 +0600

    get all data to database

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5losn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function ran() {
+    try{
+        await client.connect();
+        const partsCollection = client.db('computer_parts').collection('parts');
 
+        app.get('/parts', async(req,res) => {
+            const parts = await partsCollection.find().toArray();
+            res.send(parts);
+        })
+    }
+    finally{}
+}
+
+ran().catch(console.dir)
 
 app.get('/', (req,res) => {
     res.send('Hello parts')
