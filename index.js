08dcda9dda018fa5405c2c8e2ab21ee0c469be01
Junayed5 <mdjunayed352003@gmail.com>
2022-05-23 15:16:55 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5losn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 app.get('/', (req,res) => {
