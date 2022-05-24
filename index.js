38d00574c543c06d455a099e3ff88becd627358b
Junayed5 <mdjunayed352003@gmail.com>
2022-05-24 15:38:20 +0600

    get order

--- index.js
+++ index.js
@@ -16,28 +16,41 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function ran() {
-    try{
+    try {
         await client.connect();
         const partsCollection = client.db('computer_parts').collection('parts');
+        const purchaseOrderCollection = client.db('computer_parts').collection('orders');
 
-        app.get('/parts', async(req,res) => {
+        app.get('/parts', async (req, res) => {
             const parts = await partsCollection.find().toArray();
             res.send(parts);
         })
 
-        app.get('/part/:id', async(req,res) => {
+        app.get('/part/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id:ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const result = await partsCollection.findOne(filter);
             res.send(result);
         })
+
+
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await purchaseOrderCollection.insertOne(orders);
+            res.send(result);
+        })
+
+        app.get('/orders', async(req,res) => {
+            const orders = await purchaseOrderCollection.find().toArray();
+            res.send(orders);
+        } )
     }
-    finally{}
+    finally { }
 }
 
 ran().catch(console.dir)
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello parts')
 })
 
